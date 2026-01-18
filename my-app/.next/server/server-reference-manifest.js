2305e4635062ff7ea1f287b18b5cd8373994b9cf
Travis <36664554+Travh98@users.noreply.github.com>
2026-01-18 15:06:41 -0500

    Remove .next

--- my-app/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"tpZtNpbehVGncoAfjb9Sp4+OgjaOMmhNyJqBWIL2Sms=\"\n}"
--- my-app/.next/static/chunks/app_page_tsx_149f6574._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_de3433e4._.js"
-],
-    source: "dynamic"
-});
